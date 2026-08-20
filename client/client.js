243da5104c0d045da6179450523397c1f72cee7b
GreenOnion <63573333+bluelightgit@users.noreply.github.com>
2026-08-21 00:54:43 +0800

    fix: align client loader with scoped package

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 window.__ModuleLoader__.load({
-	id: "dsh-remote-access",
+	id: "@greenonion/dsh-remote-access",
 	factory: (require) => {
 		var module = { exports: {} };
 		var exports = module.exports;
